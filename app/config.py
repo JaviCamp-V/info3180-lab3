041b862c1ef5d38e7f5871761d7a992f7a1b2fa4
JaviCamp-V <54679525+JaviCamp-V@users.noreply.github.com>
2021-02-21 19:40:52 -0500

    finished

--- app/config.py
+++ app/config.py
@@ -2,12 +2,14 @@ import os
 
 class Config(object):
     """Base Config Object"""
-    DEBUG = False
+    DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
     MAIL_PORT = os.environ.get('MAIL_PORT') or '25'
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USE_TLS=True
+    MAIL_USE_SSL=False
    
 class DevelopmentConfig(Config):
     """Development Config that extends the Base Config Object"""
